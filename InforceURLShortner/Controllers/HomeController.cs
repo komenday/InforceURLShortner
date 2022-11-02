828602afc4fa68162e92acefbd30ac607a3a6477
Yurii Komenda <komenday2gmail.com>
2022-11-02 20:05:51 +0200

    Access to the functionality was divided

--- InforceURLShortner/Controllers/HomeController.cs
+++ InforceURLShortner/Controllers/HomeController.cs
@@ -9,16 +9,14 @@ namespace InforceURLShortner.Controllers
     public class HomeController : Controller
     {
         InforceShortnerContext _context;
-        public HomeController(InforceShortnerContext context)
-        {
-            _context = context;
-        }
+        public HomeController(InforceShortnerContext context) => _context = context;
 
         public async Task<IActionResult> Index()
         {
             return View(await _context.Urls.ToListAsync());
         }
 
+        [Authorize]
         [HttpGet]
         public IActionResult Create()
         {
@@ -29,13 +27,60 @@ namespace InforceURLShortner.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(string fullUrl)
         {
-            Url url = new Url {
-                FullURL = fullUrl,
-                ShortURL = UrlShortner.IdToShortURL(fullUrl.GetHashCode()),
-                AuthorLogin = User.Identity.Name,
-                CreationDate = DateTime.Now
-            };
-            _context.Add(url);
+            if (_context.Urls.Where(u => u.FullURL == fullUrl).Count() == 0)
+            {
+                Url url = new Url
+                {
+                    FullURL = fullUrl,
+                    ShortURL = UrlShortner.IdToShortURL(fullUrl.GetHashCode()),
+                    AuthorLogin = User.Identity.Name,
+                    CreationDate = DateTime.Now
+                };
+                _context.Add(url);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var url = await _context.Urls.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (url == null)
+                return NotFound();
+
+            return View(url);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var url = await _context.Urls.FirstOrDefaultAsync(u => u.Id == id);
+
+            if (url == null)
+                return NotFound();
+
+            if (User.IsInRole("admin") || User.Identity.Name == url.AuthorLogin)
+                return View(url);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            Url url = await _context.Urls.FindAsync(id);
+            _context.Urls.Remove(url);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
