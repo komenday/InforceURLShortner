6e7aebe27a5d836864f39a5b3db8c4f35a0151c0
Yurii Komenda <komenday2gmail.com>
2022-11-03 12:21:37 +0200

    Refactored chapter "About"
    All logic of "About" was moved to separate controller

---
+++ InforceURLShortner/Controllers/AboutController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InforceURLShortner.Controllers
+{
+    public class AboutController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> About()
+        {
+            string path = @"C:\Users\komen\source\repos\InforceURLShortner\InforceURLShortner\wwwroot\about.txt";
+            string text = Environment.NewLine;
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                text = await reader.ReadToEndAsync();
+            }
+            return View((object)text);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "admin")]
+        public IActionResult EditAbout(string innerText)
+        {
+            return View((object)innerText);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> RecordEditAbout(string edited)
+        {
+            string path = @"C:\Users\komen\source\repos\InforceURLShortner\InforceURLShortner\wwwroot\about.txt";
+            using (StreamWriter writer = new StreamWriter(path, false))
+            {
+                await writer.WriteLineAsync(edited);
+            }
+            return RedirectToAction(nameof(About));
+        }
+    }
+}
